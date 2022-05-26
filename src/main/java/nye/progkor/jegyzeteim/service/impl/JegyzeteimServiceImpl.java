05892675e4a42dd4405cf4d2837dc635f0d2e55e
pethotibor <petho.tibor@gmail.com>
2022-05-26 22:16:19 +0200

    modified JegyzeteimServiceImpl

--- src/main/java/nye/progkor/jegyzeteim/service/impl/JegyzeteimServiceImpl.java
+++ src/main/java/nye/progkor/jegyzeteim/service/impl/JegyzeteimServiceImpl.java
@@ -4,35 +4,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import nye.progkor.jegyzeteim.model.Jegyzeteim;
 
 import nye.progkor.jegyzeteim.model.exception.NotFoundException;
 import nye.progkor.jegyzeteim.service.JegyzeteimService;
 
-
 @Service
-public class JegyzeteimServiceImpl implements JegyzeteimService{
+public class JegyzeteimServiceImpl implements JegyzeteimService {
 
 	private final List<Jegyzeteim> dataBase = new ArrayList<>();
-	
+
 	@Autowired
-	public JegyzereimServiceImpl() {
+	public JegyzeteimServiceImpl() {
 		dataBase.add(new Jegyzeteim(1L, "Megnezendo filmek", "A galaxis őrzői\nFlash - A Villám\nLegenda vagyok"));
 		dataBase.add(new Jegyzeteim(2L, "Bevaslo lista", "Tej\nCukor\nTojas\nKenyer"));
 	}
-	
+
 	public JegyzeteimServiceImpl(final List<Jegyzeteim> jegyzeteim) {
-		dataBase.add(jegyzeteim);
+		dataBase.addAll(jegyzeteim);
 	}
-	
-	
-	
+
 	@Override
 	public List<Jegyzeteim> getAllJegyzeteim() {
 		// TODO Auto-generated method stub
-		return Collections.unmodifiableList(DATA_BASE);
+		return Collections.unmodifiableList(dataBase);
 	}
 
 	@Override
@@ -41,7 +39,7 @@ public class JegyzeteimServiceImpl implements JegyzeteimService{
 		return dataBase.stream()
 				.filter(jegyzeteim -> jegyzeteim.getId().equals(id))
 				.findFirst()
-				.orElseThrow(NotFoundException::new));
+				.orElseThrow(NotFoundException::new);
 	}
 
 	@Override
@@ -52,7 +50,7 @@ public class JegyzeteimServiceImpl implements JegyzeteimService{
 	}
 
 	@Override
-	public Jegyzeteim updateJegyzeteim(final Long id,final Jegyzeteim jegyzeteimChange) {
+	public Jegyzeteim updateJegyzeteim(final Long id, final Jegyzeteim jegyzeteimChange) {
 		final Jegyzeteim jegyzeteim = getJegyzeteim(id);
 		jegyzeteim.setTitle(jegyzeteim.getTitle());
 		jegyzeteim.setNote(jegyzeteim.getNote());
@@ -63,19 +61,18 @@ public class JegyzeteimServiceImpl implements JegyzeteimService{
 	public void deleteJegyzeteim(final Long id) {
 		final Jegyzeteim jegyzeteim = getJegyzeteim(id);
 		dataBase.remove(jegyzeteim);
-	
-	
+
 	}
 
 	private long getNextId() {
 		return getLastId() + 1L;
 	}
-	
-	private long gtLastId() {
+
+	private long getLastId() {
 		return dataBase.stream()
 				.mapToLong(Jegyzeteim::getId)
 				.max()
 				.orElse(0);
 	}
-	
+
 }
