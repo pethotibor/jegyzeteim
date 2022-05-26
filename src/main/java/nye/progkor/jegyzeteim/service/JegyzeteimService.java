e7408b620510f4e529acf962e2032549b903a692
pethotibor <petho.tibor@gmail.com>
2022-05-26 22:15:48 +0200

    modified JegyzeteimService

--- src/main/java/nye/progkor/jegyzeteim/service/JegyzeteimService.java
+++ src/main/java/nye/progkor/jegyzeteim/service/JegyzeteimService.java
@@ -1,5 +1,20 @@
 package nye.progkor.jegyzeteim.service;
 
-public class JegyzeteimService {
+import java.util.List;
 
+import nye.progkor.jegyzeteim.model.Jegyzeteim;
+
+public interface JegyzeteimService {
+
+	List<Jegyzeteim> getAllJegyzeteim();
+	
+	Jegyzeteim getJegyzeteim(Long id);
+	
+	Jegyzeteim createJegyzeteim(Jegyzeteim jegyzeteim);
+	
+	Jegyzeteim updateJegyzeteim(Long id, Jegyzeteim jegyzeteimChange);
+	
+	void deleteJegyzeteim(Long id);
+	
+	
 }
