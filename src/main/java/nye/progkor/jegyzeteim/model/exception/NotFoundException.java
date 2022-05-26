a456bc111d0a6900fe8ab507fcefe970e7574353
pethotibor <petho.tibor@gmail.com>
2022-05-26 22:11:15 +0200

    modified exception

--- src/main/java/nye/progkor/jegyzeteim/model/exception/NotFoundException.java
+++ src/main/java/nye/progkor/jegyzeteim/model/exception/NotFoundException.java
@@ -1,5 +1,20 @@
 package nye.progkor.jegyzeteim.model.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
 
 }
